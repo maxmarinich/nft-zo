c3dc5cd88367181c2741965aae24c8059bab9dcf
Maksim Marinich <mmarinich@Admins-MacBook-Pro.local>
2021-04-27 14:39:41 +0300

    upd scripts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,10 @@
 async function main() {
   const tokenUri = 'https://maxmarinich.github.io/nft-zo/zo/';
   const contractUri = 'https://maxmarinich.github.io/nft-zo/contract';
-  const [{ address, getBalance }] = await ethers.getSigners();
+  const [deployer] = await ethers.getSigners();
 
-  console.log(
-    `Deploying contract with the account: ${address}.
-     Account balance: ${(await getBalance()).toString()}.
-    `
-  );
+  console.log(`Deploying contract with the account: ${deployer.address}.`);
+  console.log(`Account balance: ${(await deployer.getBalance()).toString()}.`);
 
   const NZO = await ethers.getContractFactory('ZoFactory');
   const nzo = await NZO.deploy(tokenUri, contractUri);
--- scripts/mint.js
+++ scripts/mint.js
@@ -4,13 +4,13 @@ async function main() {
   const NZO = await ethers.getContractFactory("ZoFactory")
   const nzo = await NZO.attach(alchemyContractAddress)
 
-  await nzo.setBaseURI('https://maxmarinich.github.io/nft-zo/zo/');
-
-  const value = await nzo.baseTokenURI();
+  const baseURI = await nzo.baseTokenURI();
+  const contractURI = await nzo.contractURI();
   const id = await nzo.createZombie();
 
-  console.log('NZO URI:', value);
-  console.log('NZO ID:', id);
+  console.log('NZO baseURI:', baseURI);
+  console.log('NZO contractURI:', contractURI);
+  console.log('NZO zombieID:', id);
 }
 
 main()
