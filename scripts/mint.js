0313f276f966c1d81a2d38429c3b5dc061f82c73
Maksim Marinich <mmarinich@Admins-MacBook-Pro.local>
2021-05-06 19:59:45 +0300

    upd

--- scripts/mint.js
+++ scripts/mint.js
@@ -6,7 +6,7 @@ async function main() {
 
   const baseURI = await contract.baseTokenURI();
   const contractURI = await contract.contractURI();
-  const zombie = await contract.createZombie();
+  const zombie = await contract._createZombie('Tony', 'ZOP', 'LOLOLOLO', 'POL', 'SUB-1');
 
   console.log('NZO baseURI:', baseURI);
   console.log('NZO contractURI:', contractURI);
