b66e664c68a82e351c31f462cdeb6802cc314228
Maksim Marinich <mmarinich@Admins-MacBook-Pro.local>
2021-05-13 18:23:53 +0300

    setup

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,16 @@
 async function main() {
-  const tokenUri = 'https://maxmarinich.github.io/nft-zo/zo-multiple/';
-  const contractUri = `${tokenUri}contract`;
+  const baseUrl = 'https://maxmarinich.github.io/nft-zo'
+  const baseTokenUri = `${baseUrl}/items/`;
+  const baseContractUri = `${baseUrl}/contract`;
+  const totalSupplyLimit = 3;
+
   const [deployer] = await ethers.getSigners();
 
   console.log(`Deploying contract with the account: ${deployer.address}.`);
   console.log(`Account balance: ${(await deployer.getBalance()).toString()}.`);
 
   const contractFactory = await ethers.getContractFactory('ZoFactory');
-  const contract = await contractFactory.deploy(tokenUri, contractUri, 420);
+  const contract = await contractFactory.deploy(baseTokenUri, baseContractUri, totalSupplyLimit);
 
   await contract.deployed();
   console.log('Deployed to:', contract.address);
--- scripts/transfer.js
+++ scripts/transfer.js
@@ -1,16 +1,12 @@
-const { ownerAddress, alchemyContractAddress } = require('../secrets.json');
+const { alchemyContractAddress } = require('../secrets.json');
 
 async function main() {
-  const owner = ownerAddress;
   const newOwner = '0xA696906271b10DA4D6ABf13828Ee787d597b2566';
-  // const newOwner = '0xa190a41edA88931A92468065903C8FA908611892';
-  const tokenId = 1;
-
   const contractFactory = await ethers.getContractFactory('ZoFactory');
   const contract = await contractFactory.attach(alchemyContractAddress);
 
-  const data = await contract.transferFrom(owner, newOwner, tokenId);
-  console.log('NZO transfer success:', data);
+  const tokenID = await contract.transferToken(newOwner, 1);
+  console.log('NZO transfer success:', tokenID);
 }
 
 main()
