413d6689f04d21943373906a705b5e85c42463f1
Maksim Marinich <mmarinich@Admins-MacBook-Pro.local>
2021-05-21 11:45:30 +0300

    upd deps

--- base.config.js
+++ base.config.js
@@ -1,4 +1,4 @@
-const baseUrl = 'https://maxmarinich.github.io/nft-zo';
+const baseUrl = 'https://maxmarinich.github.io/nft-zo/api';
 const baseTokenUri = `${baseUrl}/items/`;
 const baseContractUri = `${baseUrl}/contract`;
 const totalSupplyLimit = 10;
